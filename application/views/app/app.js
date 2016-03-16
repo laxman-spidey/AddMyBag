36642a552505ff33ab546f70a7d9dc8682c9d6e2
Laxman Spidey <mittu.thefire@gmail.com>
2016-03-16 16:20:29 +0000

    test html added

--- application/views/app/app.js
+++
@@ -1,1 +0,0 @@
-var AddMyBag = angular.module("mohApp", ['ngRoute']);
---
+++ application/views/test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AddMyBag</title>
+        <script type="text/javascript" src="<?php echo APPPATH;?>Scripts/app/app.js"></script>
+    </head>
+    <body>
+        test welcome_message
+    </body>
+</html>
