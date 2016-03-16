168ed4b07d3d137cc2a2c9eba0176c4fbafe2e9a
Laxman Spidey <mittu.thefire@gmail.com>
2016-03-16 17:15:15 +0000

    working script

--- application/views/test.php
+++ application/views/test.php
@@ -2,7 +2,10 @@
 <html>
     <head>
         <title>AddMyBag</title>
-        <script type="text/javascript" src="<?php echo APPPATH;?>Scripts/app/app.js"></script>
+        <script type="text/javascript" src="Scripts/app/LoginController.js"></script>
+        <script type="text/javascript" src="Scripts/app/app.js"></script>
+        
+        
     </head>
     <body>
         test welcome_message
